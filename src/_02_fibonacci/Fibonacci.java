fe4cfa83c751ef9f3936cae8fc5e6c43a1c278d2
League Student <league@iMac-22.attlocal.net>
2020-03-09 19:55:47 -0700

    FINISHED FIBONACCI

--- src/_02_fibonacci/Fibonacci.java
+++ src/_02_fibonacci/Fibonacci.java
@@ -3,8 +3,19 @@ package _02_fibonacci;
 public class Fibonacci {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+int num1 = 1;
+int num2 = 1;
+int sum = 0;
+System.out.println(num1);
+System.out.println(num2);
+for(int i = 0;i<12;i++ ) {
+	
+	sum =num1+num2;
+	num1=num2;
+num2=sum;
 
+System.out.println(sum);
+}
 	}
 
 }
